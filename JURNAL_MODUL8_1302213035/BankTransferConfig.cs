1628963dfc2ca2d132e673a00fd80b5aa3b469c9
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-04-12 17:00:08 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ JURNAL_MODUL8_1302213035/BankTransferConfig.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JURNAL_MODUL8_1302213035
+
+{
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+        public string meth1 { get; set; }
+        public string meth2 { get; set; }
+        public string meth3 { get; set; }
+        public string meth4 { get; set; }
+
+        public string en { get; set; }
+        public string id { get; set; }
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, int threshold,
+            int low_fee, int high_fee, string meth1, string meth2,
+            string meth3, string meth4, string en, string id)
+        {
+            this.lang = lang;
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+            this.meth1 = meth1;
+            this.meth2 = meth2;
+            this.meth3 = meth3;
+            this.meth4 = meth4;
+            this.en = en;
+            this.id = id;
+        }
+
+        public void changeLang_1302213035()
+        {
+            if (lang == "en")
+            {
+                lang = "id";
+            }
+            else
+            {
+                lang = "en";
+            }
+        }
+    }
+
+    class readJson
+    {
+        public BankTransferConfig bankTfCon;
+        public const string readFile = "C:\\Users\\asus\\Documents\\TUGAS ADEK\\SEMESTER 4\\KPL\\JURNAL\\JURNAL_MODUL8_1302213035\\Program.cs" + 
+            "C:\\Users\\asus\\Documents\\TUGAS ADEK\\SEMESTER 4\\KPL\\JURNAL\\JURNAL_MODUL8_1302213035\\BankTransferConfig.cs" +
+            "C:\\Users\\asus\\Documents\\TUGAS ADEK\\SEMESTER 4\\KPL\\JURNAL\\JURNAL_MODUL8_1302213035\\Bank_Transfer_Config.json"
+            ;
+        public readJson()
+        {
+            try
+            {
+                readConfigFile_1302213035();
+            }
+            catch (Exception)
+            {
+                setDefault_1302213035();
+                writeNewConfigFile_1302213035();
+            }
+        }
+
+        private BankTransferConfig readConfigFile_1302213035()
+        {
+            string configJson = File.ReadAllText(readFile);
+            bankTfCon = JsonSerializer.Deserialize<BankTransferConfig>(configJson);
+            return bankTfCon;
+        }
+
+        private void setDefault_1302213035()
+        {
+            int threshold = 25000000;
+            int low_fee = 6500;
+            int high_fee = 15000;
+            string meth1 = "1. RTO (real-time)";
+            string meth2 = "2. SKN";
+            string meth3 = "3. RTGS";
+            string meth4 = "4. BI FAST";
+            string en = "yes";
+            string id = "ya";
+            bankTfCon = new BankTransferConfig("id", threshold, low_fee,
+                high_fee, meth1, meth2, meth3, meth4, en, id);
+        }
+
+        private void writeNewConfigFile_1302213035()
+        {
+            JsonSerializerOptions opt = new JsonSerializerOptions() { WriteIndented = true };
+
+            string jsonStr = JsonSerializer.Serialize(bankTfCon, opt);
+        }
+    }
+}
---
+++ JURNAL_MODUL8_1302213035/Program.cs
@@ -0,0 +1,46 @@
+﻿using JURNAL_MODUL8_1302213035;
+using System.Security.Cryptography.X509Certificates;
+
+BankTransferConfig tfConfig = new BankTransferConfig();
+readJson read = new readJson();
+
+// read.bankTfCon.changeLang_1302213035();
+
+// part i & ii
+Console.WriteLine("Please insert the amount of money to transfer:");
+int mon = int.Parse(Console.ReadLine());
+
+int sum = 0;
+if (mon <= read.bankTfCon.threshold)
+{
+    sum = mon + read.bankTfCon.low_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.low_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+}
+else
+{
+    sum = mon + read.bankTfCon.high_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.high_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+}
+
+// part iii & iv
+Console.WriteLine("\nSelect transfer method: ");
+Console.WriteLine(read.bankTfCon.meth1);
+Console.WriteLine(read.bankTfCon.meth2);
+Console.WriteLine(read.bankTfCon.meth3);
+Console.WriteLine(read.bankTfCon.meth4);
+string meth = Console.ReadLine();
+
+// part v - vii
+Console.WriteLine("\nPlease type " + read.bankTfCon.en + " to confirm the transaction");
+string confirm = Console.ReadLine();
+
+if (confirm == read.bankTfCon.en)
+{
+    Console.WriteLine("The transfer is completed");
+}
+else
+{
+    Console.WriteLine("Transfer is cancelled");
+}
